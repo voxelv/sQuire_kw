1a2160411743c7625b348c3d582eee31969d148c
Jesse Jutson <jcjutson@gmail.com>
2016-03-25 10:57:20 -0700

    Updated some filenames

--- Database/Create Projects.sql
+++ Database/Create Projects.sql
@@ -3,23 +3,23 @@ use squiredb;
 
 create table PFiles (
 	pfid integer not null primary key auto_increment,
-    name varchar(30);
-	project integer;
+    pfname varchar(30),
+	pid integer,
 	timeCreated timestamp,
     creatorID integer,
-	foreign key (project) references Projects(PID),
+	foreign key (pid) references Projects(PID),
     foreign key (creatorID) references Users(userID)
 );
 
 create table Projects (
 	PID integer not null primary key auto_increment,
-	name varchar(30),
+	pname varchar(30),
 	location varchar(40)
 );
 
 create table ProjectAccess (
 	PID integer not null primary key,
-	userID integer not null primary key
+	userID integer not null primary key,
 	foreign key (PID) references Projects(PID),
 	foreign key (userID) references Users(userID)
 );
--- Database/Create Users.sql
+++ Database/Create Users.sql
@@ -1,3 +1,4 @@
+create database squiredb;
 
 use squiredb;
 
