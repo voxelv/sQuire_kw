bbbc7d2723d446cea1411b5fb74ff09cd2b7a039
Feng Guan <545576676@qq.com>
2016-03-24 18:08:30 -0700

    Update HelloWorld.java

--- prototypes/guan2264/HelloWorld.java
+++ prototypes/guan2264/HelloWorld.java
@@ -14,31 +14,50 @@ public class HelloWorld
     		while(!isExit){
     			System.out.println("***File Manager***");
 				System.out.println("1. Create File");
-				System.out.println("2. Move File");
+				System.out.println("2. Print Directory");
 				System.out.println("3. Remove File");
 				System.out.print("Enter your choice: ");
 				
 	    		Scanner reader = new Scanner(System.in);
 				int menuInput = reader.nextInt();		
-//				Thread.sleep(1000);
+//				Thread.sleep(100);
+				String desktop = System.getProperty ("user.home") + "/Desktop/";
 
 				switch(menuInput){
 					case 1:
 						System.out.println("Enter the filename: ");
 						String fileName = reader.next();
-						String desktop = System.getProperty ("user.home") + "/Desktop/";
 
 	    				File file = new File(desktop+fileName+"");
-	      
 	    				if (file.createNewFile()){
-	    				System.out.println("File "+fileName+" is created!");
+	    					System.out.println("File "+fileName+" is created!");
 	    				}else{
 	    	    			System.out.println("File "+fileName+" already exists.");
 	    				}
 
 
 						break;
+					case 2:
+						File folder = new File(desktop);
+						File[] listOfFiles = folder.listFiles();
+	    	    		System.out.println("-----Printing-----");
+	    	    		
+	    	    		
+
+    					for (int i = 0; i < listOfFiles.length; i++) {
+      						if (listOfFiles[i].isFile()) {
+        						System.out.println("-File " + listOfFiles[i].getName());
+      						} else if (listOfFiles[i].isDirectory()) {
+        						System.out.println("-Directory " + listOfFiles[i].getName());
+      						}
+    					}
+	    	    		System.out.println("---End of print---");
+
+						break;
+					case 3:
+						break;
 					default:
+						System.out.println("Please enter the valid number.");
 						break;
 				}
 
