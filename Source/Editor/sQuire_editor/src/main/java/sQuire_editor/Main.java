a4b02fe17d3b7f01ccdd39896d02301178af5d17
Tim <legocacherx@gmail.com>
2016-03-29 18:54:31 -0700

    first commit

---
+++ Source/Editor/sQuire_editor/src/main/java/sQuire_editor/Main.java
@@ -0,0 +1,13 @@
+/* Main.java
+ * 
+ * Created by: Tim
+ * On: Mar 29, 2016 at 6:52:01 PM
+*/
+
+package sQuire_editor;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
