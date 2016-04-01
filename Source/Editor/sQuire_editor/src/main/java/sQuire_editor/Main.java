6e987564ec85b2f8a3122e6ebab6fd45abaf3f2b
Tim <legocacherx@gmail.com>
2016-04-01 12:57:55 -0700

    Using a HTMLEditor now. Wonder if it will work for us

--- Source/Editor/sQuire_editor/src/main/java/sQuire_editor/Main.java
+++ Source/Editor/sQuire_editor/src/main/java/sQuire_editor/Main.java
@@ -5,9 +5,27 @@
 */
 
 package sQuire_editor;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.HTMLEditor;
+import javafx.stage.Stage;
 
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Editor Test");
+		
+		HTMLEditor htmlEditor = new HTMLEditor();
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(htmlEditor);
+		primaryStage.setScene(new Scene(root, 400, 300));
+		primaryStage.show();
 	}
 }
