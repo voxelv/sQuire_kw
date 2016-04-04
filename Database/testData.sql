775e0beb031c88459ceb3bb70b333ebe6f36c64d
handstand2002 <grant@MacBookPro-406C8F369684.local>
2016-04-04 11:54:45 -0700

    Created test data for DB
    Created a sql file containing a bunch of test data to put into the DB

---
+++ Database/testData.sql
@@ -0,0 +1,12 @@
+INSERT INTO Users (userID, userName) values ('1', 'wade4238');
+INSERT INTO Users (userID, userName) values ('2', 'guan2264');
+INSERT INTO Users (userID, userName) values ('3', 'juts3869');
+INSERT INTO Users (userID, userName) values ('4', 'denn2725');
+INSERT INTO Users (userID, userName) values ('5', 'benz5834');
+INSERT INTO Users (userID, userName) values ('6', 'slip5295');
+INSERT INTO Users (userID, userName) values ('7', 'ferg2065');
+INSERT INTO Users (userID, userName) values ('8', 'ocke8865');
+
+INSERT INTO Channels (channelID, channelName) values ('1', 'Global');
+INSERT INTO Channels (channelID, channelName) values ('2', 'Project1');
+
