a83ee321d663ab2d1771b23f749c7f42fc75283d
Tim <legocacherx@gmail.com>
2016-04-07 21:52:38 -0700

    it's because getScene() returns null...

--- GUI/sQuire/src/main/java/sQuire_editor/DiffCodeArea.java
+++ GUI/sQuire/src/main/java/sQuire_editor/DiffCodeArea.java
@@ -12,9 +12,36 @@ import org.fxmisc.richtext.StyleSpansBuilder;
 
 public class DiffCodeArea extends CodeArea{
 	
+    private static final String sampleCode = String.join("\n", new String[] {
+            "package com.example;",
+            "",
+            "import java.util.*;",
+            "",
+            "public class Foo extends Bar implements Baz {",
+            "",
+            "    /*",
+            "     * multi-line comment",
+            "     */",
+            "    public static void main(String[] args) {",
+            "        // single-line comment",
+            "        for(String arg: args) {",
+            "            if(arg.length() != 0)",
+            "                System.out.println(arg);",
+            "            else",
+            "                System.err.println(\"Warning: empty string as argument\");",
+            "        }",
+            "    }",
+            "",
+            "}"
+        });
+	
 	public DiffCodeArea() {
 		super();
+        this.setParagraphGraphicFactory(LineNumberFactory.get(this));
+		this.replaceText(sampleCode);
 		System.out.println("DiffCodeArea creation");
+        if(getScene() == null) System.out.println("SCENE is NULL"); 
+        else System.out.println("Scene not null?");
 	}
 
 }
