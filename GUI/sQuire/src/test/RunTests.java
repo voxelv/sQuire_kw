3cbb4e2bb5976c8cbf3e39e16ab24db78769aa1c
Tim Slippy, voxelv <slippytim@gmail.com>
2016-04-22 09:18:32 -0700

    adding some unit test stuffs

---
+++ GUI/sQuire/src/test/RunTests.java
@@ -0,0 +1,17 @@
+/* RunTests.java
+ * 
+ * Created by: Tim
+ * On: Apr 21, 2016 at 5:05:04 PM
+*/
+
+package test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class RunTests {
+  public static void main(String[] args) {
+    System.out.println("HEY");
+  }
+}
---
+++ GUI/sQuire/src/test/TestEditorCodeArea.java
@@ -0,0 +1,20 @@
+/* TestEditorCodeArea.java
+ * 
+ * Created by: Tim
+ * On: Apr 21, 2016 at 10:30:35 AM
+*/
+
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEditorCodeArea {
+	
+	@Test
+	public void testEditorCodeArea() {
+		System.out.println("Wow such test.");
+		assertEquals("Test Message", "Test Message DERP");
+	}
+}
